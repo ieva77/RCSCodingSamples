e5a588a6741e40016cb2fa7633a67faa780e3e7b
ieva77 <stegmane.ieva@gmail.com>
2017-12-21 02:33:02 +0200

    koments

---
+++ RCS2017/Calculator/Calculations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculations
+    {
+        public int AskUserForNumber()
+        {
+            Console.WriteLine("please enter number:");
+            int number;
+            string inputText = Console.ReadLine();
+            number = Int32.Parse(inputText);
+            return number;
+        }
+    }
+}
---
+++ RCS2017/Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Programaskaitīt
+    {
+        static void Main(string[] args)
+        {
+            // izveido kalkulēsanas ojektu
+            Calculations calc;
+            calc = new Calculations();
+
+            // parasīt lietotājam vērtību
+            int firstNumber = calc.AskUserForNumber();
+            // paprasīt otra vērtība
+            int secondNumber = calc.AskUserForNumber();
+
+            // saskaitīt
+            int result = firstNumber + secondNumber;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
